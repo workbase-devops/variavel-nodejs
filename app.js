a1261cb9d04ed3b3f9753506db60f0eed3ee99ac
workbase-devops <117115104+workbase-devops@users.noreply.github.com>
2025-10-31 20:22:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const newRelicKey = process.env.NEW_RELIC_LICENSE_KEY;
+const sonarToken = process.env.SONAR_TOKEN;
+
+if (!process.env.API_KEY) {
+  throw new Error('API_KEY não definida!');
+}
+
 console.log('Ambiente:', process.env.NODE_ENV);
 console.log('Chave API:', process.env.API_KEY);
 console.log('Variável customizada:', process.env.CUSTOM_VAR);
